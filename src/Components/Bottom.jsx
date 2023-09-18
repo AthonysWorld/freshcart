26d4e09ef716b3ac3f61f5b3fb471f65eeec040b
Adams Anthony Isioma <114524110+AthonysWorld@users.noreply.github.com>
2023-09-18 09:21:50 +0100

    Add files via upload

--- src/Components/Bottom.jsx
+++ src/Components/Bottom.jsx
@@ -4,21 +4,21 @@ const Bottom = () => {
   return (
     <div>
       <div className="Bottom container-fluid row">
-<div className="container col-lg-4">
+<div className="container pad col-lg-4">
 <h3>Get to know us</h3>
 <p>About</p>
 <p>Blog</p>
 <p>Help Center</p>
 </div>
 
-<div className=" container col-lg-4">
+<div className=" container pad col-lg-4">
 <h3>Categories</h3>
 <p>Vegetables & Fruits</p>
 <p>Breakfast & instant food</p>
 <p>Bakery & Biscuits</p>
 </div>
 
-<div className="container col-lg-4">
+<div className="container pad col-lg-4">
 <h3>FreshCart programs</h3>
 <p>Promos & Coupons</p>
 <p>Freshcart programs</p>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -10,25 +10,25 @@ const Footer = () => {
       
 <div className="container-fluid row">
 
-<div className="container col-lg-3 col-md-3">
+<div className="container pad col-lg-3 col-md-3">
 <h1><FaClock className='icons'/></h1>
 <h3>10 minute grocery now</h3>
 <p className="grey">Get your order delivered to your doorstep at the earliest from FreshCart pickup stores near you.</p>
 </div>
 
-<div className="container col-lg-3 col-md-3">
+<div className="container pad col-lg-3 col-md-3">
 <h1><FaGift className='icons'/></h1>
 <h3>Best Prices & Offers</h3>
 <p className="grey">Cheaper prices than your local supermarket, great cashback offers to top it off. Get best pricess & offers.</p>
 </div>
 
-<div className="container col-lg-3 col-md-3">
+<div className="container pad col-lg-3 col-md-3">
 <h1><FaBox className='icons'/></h1>
 <h3>Wide Assortment</h3>
 <p className="grey">Choose from 5000+ products across food, personal care, household, bakery, veg and non-veg & other categories.</p>
 </div>
 
-<div className="container col-lg-3 col-md-3">
+<div className="container pad col-lg-3 col-md-3">
 <h1><FaRocket className='icons'/></h1>
 <h3>Easy Returns</h3>
 <p className="grey">Not satisfied with a product? Return it at the doorstep & get a refund within hours. No questions asked policy</p>
