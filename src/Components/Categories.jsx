f43d9af865b44b98935a6408e4e3cd5b69d8e02f
Adams Anthony Isioma <114524110+AthonysWorld@users.noreply.github.com>
2023-09-18 13:25:52 +0100

    Add files via upload

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -11,14 +11,14 @@ Aos.init({duration:2000});
     <div>
     <div className='category'>
     <h2 className="slide-span">Featured Categories</h2>
-      <div className='row category-row'>
-<div className='col-lg-5 col-md-5 col-sm-5 container category1' data-aos="zoom-out-up">
+      <div className='row category-row' data-aos="zoom-in">
+<div className='col-lg-5 col-md-5 col-sm-5 container category1'>
 <h2>Fruits & Vegetables</h2>
 <p>Get up to 30% off</p>
 <button className='btn btn1'>Shop now <FaArrowRight/></button>
 </div>
 
-<div className='col-lg-5 col-md-5 col-sm-5 container category2' data-aos="zoom-out-up">
+<div className='col-lg-5 col-md-5 col-sm-5 container category2'>
 <h2>Freshly Baked Buns</h2>
 <p>Get up to 20% off</p>
 <button className='btn btn1'>Shop now <FaArrowRight/></button>
